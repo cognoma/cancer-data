67f80326c4514d1b0d4d42a744fd85b971c24f7f
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-09-07 10:55:14 -0500

    Add exploratory analyses of mutation data (#22)
    * Add exploratory analyses of mutation data
    
    Based on a preliminary notebook we created at the 2016-08-23 Cognoma Meetup
    (https://www.meetup.com/DataPhilly/events/233403001/).
    
    * Add genes to mutation frequency by disease heatmap

---
+++ scripts/3.explore-mutations.py
@@ -0,0 +1,119 @@
+
+# coding: utf-8
+
+# # Exploratory data analysis of TCGA mutation data
+
+# In[1]:
+
+import os
+
+import numpy
+import pandas
+import seaborn
+
+get_ipython().magic('matplotlib inline')
+
+
+# ## Read TCGA datasets
+
+# In[2]:
+
+path = os.path.join('data', 'mutation-matrix.tsv.bz2')
+mutation_df = pandas.read_table(path, index_col=0)
+mutation_df.columns.name = 'entrez_gene_id'
+mutation_df.shape
+
+
+# In[3]:
+
+path = os.path.join('data', 'samples.tsv')
+sample_df = pandas.read_table(path)
+sample_df.head(2)
+
+
+# ## Distribution of mutations counts for genes
+
+# In[4]:
+
+gene_df = mutation_df.sum(axis='rows').rename('n_mutations').reset_index()
+gene_df['n_mutations_log1p'] = numpy.log1p(gene_df.n_mutations)
+gene_df.head(2)
+
+
+# In[5]:
+
+ax = seaborn.distplot(gene_df.n_mutations_log1p)
+xticks = ax.get_xticks()
+xticklabels = numpy.expm1(xticks).round().astype(int)
+axis_texts = ax.set_xticklabels(xticklabels)
+
+
+# In[6]:
+
+sum(gene_df.n_mutations == 0)
+
+
+# ## Distribution of mutations counts for samples
+
+# In[7]:
+
+sample_df = sample_df.merge(
+    mutation_df.sum(axis='columns').rename('n_mutations').reset_index()
+)
+sample_df['n_mutations_log1p'] = numpy.log1p(sample_df.n_mutations)
+sample_df.head(2)
+
+
+# In[8]:
+
+# Mutations per sample
+ax = seaborn.distplot(sample_df.n_mutations_log1p)
+xticks = ax.get_xticks()
+xticklabels = numpy.expm1(xticks).round().astype(int)
+axis_texts = ax.set_xticklabels(xticklabels)
+
+
+# ## Diagnosis age versus mutation count for samples
+
+# In[9]:
+
+grid = seaborn.jointplot('n_mutations_log1p', 'age_diagnosed', data=sample_df, kind='hex')
+xticks = grid.ax_marg_x.get_xticks()
+xticklabels = numpy.expm1(xticks).round().astype(int)
+axis_texts = grid.ax_marg_x.set_xticklabels(xticklabels)
+
+
+# ## Mutation frequency by disease
+
+# In[10]:
+
+genes = mutation_df.columns.tolist()
+verbose_mutation_df = sample_df.merge(mutation_df.reset_index())
+mutation_freq_df = verbose_mutation_df.groupby('disease').apply(lambda df: df[genes].mean(axis='rows')).assign(
+    n_mutations = verbose_mutation_df.groupby('disease').apply(len)
+)
+mutation_freq_df.iloc[:3, :3]
+
+
+# In[11]:
+
+gene_subset = {
+    '7157': 'TP53', # tumor protein p53
+    '7428': 'VHL', # von Hippel-Lindau tumor suppressor
+    '29126': 'CD274', # CD274 molecule
+    '672': 'BRCA1', # BRCA1, DNA repair associated
+    '675': 'BRCA2', # BRCA2, DNA repair associated
+    '238': 'ALK', # anaplastic lymphoma receptor tyrosine kinase
+    '4221': 'MEN1', # menin 1
+    '5979': 'RET', # ret proto-oncogene 
+}
+plot_df = (mutation_freq_df
+    .query("n_mutations > 100")
+    [list(gene_subset)]
+    .rename(columns=gene_subset)
+)
+
+# Convert to percent of max mutation rate for gene
+plot_df = 100 * plot_df.divide(plot_df.max())
+ax = seaborn.heatmap(plot_df)
+
