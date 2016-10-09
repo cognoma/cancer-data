eeba83d7604f45d1ae7c698ae61d44b7a867deaf
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-10-09 13:41:59 -0400

    Update cognoma/genes download location

--- scripts/0.genes-download.py
+++ scripts/0.genes-download.py
@@ -11,8 +11,8 @@ from urllib.request import urlretrieve
 
 # In[2]:
 
-repo = 'dhimmel/genes'
-commit = 'b64fcb4261005cf717d5d5ef4e03540e4a1f361e'
+repo = 'cognoma/genes'
+commit = '721204091a96e55de6dcad165d6d8265e67e2a48'
 base_url = 'https://github.com/{repo}/raw/{commit}/data/'.format(repo=repo, commit=commit)
 
 
